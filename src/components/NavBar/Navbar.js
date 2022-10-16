27e96fcc77c5890aaab2d6fabbb28b094c2447be
dazuaje <davidazuaje1084@gmail.com>
2022-10-15 22:37:27 -0300

    Adding MUI, SASS, navbar

---
+++ src/components/NavBar/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import djLogo from '../../Assets/djLogo.svg';
+import { Switch } from '@mui/material';
+import './Navbar.scss';
+
+const Navbar = () => {
+  return (
+    <nav className='nav'>
+      <ul className='nav__list'>
+
+        <img className='nav__item--logo' src={djLogo} alt="djLogo" />
+
+        <div className="nav__item__container">
+
+          <li className='nav__item'><a href="#d" className="nav__item--anchor inicioLang"> Home </a></li>
+          <li className='nav__item'><a href="#q" className="nav__item--anchor sobreLang"> About me </a></li>
+          <li className='nav__item'><a href="#d" className="nav__item--anchor habilidadesLang"> Skills </a></li>
+          <li className='nav__item'><a href="#c" className="nav__item--anchor proyectosLang"> Projects </a></li>
+          <li className='nav__item'><a href="#w" className="nav__item--anchor contactLang"> Contact </a></li>
+        </div>
+
+        <Switch defaultChecked style={{ color: '#374151' }} />
+      </ul>
+
+    </nav>
+  )
+}
+
+export default Navbar;
